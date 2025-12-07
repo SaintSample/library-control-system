78ae6e3ec03db7df36538ce8152cfa1e393060ff
Kirill Zolotarev <saint-sample@yandex.ru>
2025-12-07 13:44:10 +0300

    First blood

---
+++ src/main/java/app/computer_school/Book.java
@@ -0,0 +1,10 @@
+package app.computer_school;
+
+public class Book {
+    protected int id;
+    protected String title;
+    protected int authorId;
+    protected int publishingId;
+    protected String publishingDate;
+    protected int genreId;
+}
---
+++ src/main/java/app/computer_school/User.java
@@ -0,0 +1,85 @@
+package app.computer_school;
+
+public class User {
+    protected int id;
+    protected String firstname;
+    protected String lastname;
+    protected String middlename;
+    protected String bitrthDate;
+    protected String phone;
+    protected String email;
+
+    public User(
+            int id,
+            String firstname,
+            String lastname,
+            String middlename,
+            String bitrthDate,
+            String phone,
+            String email
+    ) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.middlename = middlename;
+        this.bitrthDate = bitrthDate;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public void setMiddlename(String middlename) {
+        this.middlename = middlename;
+    }
+
+    public String getBitrthDate() {
+        return bitrthDate;
+    }
+
+    public void setBitrthDate(String bitrthDate) {
+        this.bitrthDate = bitrthDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
